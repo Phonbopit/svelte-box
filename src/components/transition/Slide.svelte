9944f1b37e1ea829a5c1e009d7fee1f25fe1d32e
Chai Phonbopit <phonbopit@gmail.com>
2023-09-09 17:33:09 +0700

    feat(transition) - learn transition, add example transitions

---
+++ src/components/transition/Slide.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { slide } from 'svelte/transition'
+  import EmptyBox from './EmptyBox.svelte'
+
+  export let duration: number
+  export let axis: 'x' | 'y' = 'y'
+</script>
+
+<div transition:slide={{ duration, axis }}>
+  <EmptyBox name="Slide" />
+</div>
