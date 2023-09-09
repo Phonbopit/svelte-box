0205e45f83cfb1e90f02de9e516303b163eea58d
Chai Phonbopit <phonbopit@gmail.com>
2023-09-09 17:50:11 +0700

    mobile first

--- src/components/transition/EmptyBox.svelte
+++ src/components/transition/EmptyBox.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-  class="flex justify-center items-center w-48 h-48 aspect-square bg-red-400 rounded-xl shadow-xl text-white"
+  class="flex justify-center items-center w-24 h-24 md:w-48 md:h-48 aspect-square bg-red-400 rounded-xl shadow-xl text-white"
 >
   {name}
 </div>
