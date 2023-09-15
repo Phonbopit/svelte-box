d6d780d52c46b3fde598335f5600994e7ee1724a
Chai Phonbopit <Phonbopit@gmail.com>
2023-09-15 21:44:46 +0700

    chore: add svelte:head in page layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,9 @@
   import '../styles/app.css'
 </script>
 
+<svelte:head>
+  <title>Svelte Box | My Svelte Learning Journey</title>
+  <meta name="description" content="Learning & Experimenting with Svelte/SvelteKit" />
+</svelte:head>
+
 <slot />
