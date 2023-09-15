38374fa4a231d0f40845c978e76034e6f2069ea2
Chai Phonbopit <Phonbopit@gmail.com>
2023-09-15 22:23:06 +0700

    feat(ui): Add navbar / layout and separate pages

---
+++ src/lib/menus.ts
@@ -0,0 +1,14 @@
+export const menus = [
+  {
+    id: 1,
+    name: 'Transitions',
+    link: '/transitions'
+  },
+  {
+    id: 2,
+    name: 'Success Check ✅',
+    link: '/success-check'
+  }
+]
+
+export default menus
---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<div class="flex justify-center items-center h-[80vh]">
+  <span class="text-4xl text-secondary-600 transition-all animate-scale"> 🤔 Page Not Found </span>
+</div>
+
+<style>
+  @keyframes scale {
+    50% {
+      scale: 1.25;
+    }
+  }
+
+  .animate-scale {
+    animation: scale 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+  }
+</style>
